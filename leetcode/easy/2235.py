8cd8ba16baffed0f615addbb69deda28520c6595
desfox <desfox99@naver.com>
2022-09-05 16:29:02 +0900

    Solve: Leetcode 2235(Easy)

---
+++ leetcode/easy/2235.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/add-two-integers/
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+
+# 똑같은 코드를 제출해도 시간복잡도와 공간복잡도 차이가 많이 남
+# -> Leetcode에서 보여주는 시간, 공간복잡도는 너무 신경쓰지 않아도 될 듯
