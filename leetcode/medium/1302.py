013450fdbb065039b1b456882e6ea1f0ac6bb6ed
desfox <desfox99@naver.com>
2022-10-05 14:18:13 +0900

    MOD: Leetcode 1302(Medium)

--- leetcode/medium/1302.py
+++ leetcode/medium/1302.py
@@ -1,18 +1,37 @@
 # https://leetcode.com/problems/deepest-leaves-sum/
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def deepestLeavesSum(self, root):
-        def dfs1(node):
-            if not node: return 0
-            return max(dfs1(node.left), dfs1(node.right)) + 1
-        
-        def dfs2(node, d):
-            if not node: return
-            if d == depth: self.ans += node.val
-            dfs2(node.left, d+1)
-            dfs2(node.right, d+1)
-        
         self.ans = 0
-        depth = dfs1(root)
-        dfs2(root, 1)
+        self.depth = 0
+
+        # 가장 깊은 서브트리 탐색
+        def find_deepest_leaves(node):
+            if(not node): 
+                return 0
+
+            # 왼쪽과 오른쪽 서브트리 중 더 큰 레벨 + 1(현재 노드 포함)이 현재 노드의 레벨
+            return max(find_deepest_leaves(node.left), find_deepest_leaves(node.right)) + 1
+        
+        # 가장 깊은 리프 노드의 합 계산
+        def deepest_leaves_sum(node, d):
+            if(not node): 
+                return
+
+            # 가장 깊은 리프 노드의 값 축적
+            if(d == self.depth): 
+                self.ans += node.val
+
+            deepest_leaves_sum(node.left, d + 1)
+            deepest_leaves_sum(node.right, d + 1)
+    
+        self.depth = find_deepest_leaves(root)
+        deepest_leaves_sum(root, 1)
+
         return self.ans
