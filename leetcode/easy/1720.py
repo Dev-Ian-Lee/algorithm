15226c790689fce6a8c177d4aafcce93f90ee93e
desfox <desfox99@naver.com>
2022-10-05 16:22:36 +0900

    MOD: Leetcode 1720(Easy)

--- leetcode/easy/1720.py
+++ leetcode/easy/1720.py
@@ -4,9 +4,16 @@ from typing import List
 
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
-        ret = [first]
+        arr = [first]
         
         for i in range(len(encoded)):
-            ret.append(encoded[i]^ret[i])
+            arr.append(encoded[i] ^ arr[i])
             
-        return ret
+        return arr
+
+# XOR 연산은 두 비트의 값이 다르면 1, 같으면 0
+# 즉, 같은 10진수끼리의 XOR 연산 결과는 0이 됨
+# 문제에서 아래와 같은 식이 주어짐
+# encoded[i] = arr[i] ^ arr[i + 1]
+# -> encoded[i] ^ encoded[i] ^ arr[i + 1] = arr[i] ^ arr[i + 1] ^ encoded[i] ^ arr[i + 1]
+# -> arr[i + 1] = arr[i] ^ encoded[i]
