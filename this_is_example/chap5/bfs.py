3c5c10242205ec3b3e91bfd1c1d92c32be57a55d
desfox <desfox99@naver.com>
2023-01-30 22:17:47 +0900

    Solve: This is example 5-6

---
+++ this_is_example/chap5/bfs.py
@@ -0,0 +1,34 @@
+# <이것이 취업을 위한 코딩 테스트다> 예제 5-5
+
+from collections import deque
+
+def bfs(graph, root, visited):
+    queue = deque([root])
+    visited[root] = True
+
+    while queue:
+        node = queue.popleft()
+        print(node, end = ' ')
+
+        for adjacent in graph[node]:
+            if not visited[adjacent]:
+                queue.append(adjacent)
+                visited[adjacent] = True
+
+    print()
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)
