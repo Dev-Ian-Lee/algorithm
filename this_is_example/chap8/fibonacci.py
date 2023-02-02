1a62fec2af94858dbde5c3c67998a9e4159dcc60
desfox <desfox99@naver.com>
2023-02-02 17:49:09 +0900

    MOD: This is example 8-1

--- this_is_example/chap8/fibonacci.py
+++ this_is_example/chap8/fibonacci.py
@@ -7,19 +7,28 @@
     
 #     return fibonacci(x - 1) + fibonacci(x - 2)
 
-# 메모이제이션 활용한 재귀
+# 메모이제이션 활용한 재귀(탑다운)
+# memo = [0] * 100
+
+# def fibonacci(x):
+#     if x == 1 or x == 2:
+#         return 1
+    
+#     # 이미 계산되어 메모에 저장된 값은 다시 계산하지 않고 메모를 불러옴
+#     if memo[x] != 0:
+#         return memo[x]
+    
+#     # 저장되지 않은 값은 재귀적으로 계산한 뒤 메모에 저장
+#     memo[x] = fibonacci(x - 1) + fibonacci(x - 2)
+#     return memo[x]
+
+# print(fibonacci(99))
+
+# 메모이제이션을 활용한 반복(바텀업)
 memo = [0] * 100
+memo[1] = 1
+memo[2] = 1
+n = 99
 
-def fibonacci(x):
-    if x == 1 or x == 2:
-        return 1
-    
-    # 이미 계산되어 메모에 저장된 값은 다시 계산하지 않고 메모를 불러옴
-    if memo[x] != 0:
-        return memo[x]
-    
-    # 저장되지 않은 값은 재귀적으로 계산한 뒤 메모에 저장
-    memo[x] = fibonacci(x - 1) + fibonacci(x - 2)
-    return memo[x]
-
-print(fibonacci(99))
+for i in range(3, n + 1):
+    memo[i] = memo[i - 1] + memo[i - 2]
