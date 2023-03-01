3db840c28f6555eae4aea83a43a19044d268484a
desfox <desfox99@naver.com>
2023-03-01 18:49:57 +0900

    MOD: Programmers 12981(lv2)

--- programmers/level2/word_chain_game.py
+++ programmers/level2/word_chain_game.py
@@ -1,32 +1,32 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12981
 
-def solution(n, words):
-    size = len(words)
-    mem = set()
-    pre = ""
+# def solution(n, words):
+#     size = len(words)
+#     mem = set()
+#     pre = ""
     
-    for i in range(size):
-        curr = words[i]
+#     for i in range(size):
+#         curr = words[i]
         
-        if i == 0:
-            mem.add(curr)
-            pre = curr
-            continue
+#         if i == 0:
+#             mem.add(curr)
+#             pre = curr
+#             continue
         
-        # 현재 단어가 이미 나왔을 경우 탈락자 발생
-        if curr in mem:
-            return finish_game(i, n)
+#         # 현재 단어가 이미 나왔을 경우 탈락자 발생
+#         if curr in mem:
+#             return finish_game(i, n)
             
-        else:
-            if pre[-1] == curr[0]:
-                mem.add(curr)
-                pre = curr
+#         else:
+#             if pre[-1] == curr[0]:
+#                 mem.add(curr)
+#                 pre = curr
                 
-            # 이전 단어의 마지막 글자와 현재 단어의 첫 글자가 다를 경우 탈락자 발생
-            else:
-                return finish_game(i, n)
+#             # 이전 단어의 마지막 글자와 현재 단어의 첫 글자가 다를 경우 탈락자 발생
+#             else:
+#                 return finish_game(i, n)
 
-    return [0, 0]
+#     return [0, 0]
 
 # 탈락한 사람의 번호와 차례 계산
 def finish_game(i, n):
@@ -43,3 +43,19 @@ def finish_game(i, n):
         turns = ((i + 1) // n) + 1
 
     return [player, turns]
+
+# 개선 코드(set 대신 words[:i]로 중복 검사)
+def solution(n, words):
+    size = len(words)
+    
+    for i in range(1, size):
+        curr = words[i]
+        
+        if curr in words[:i]:
+            return finish_game(i, n)
+        
+        else:
+            if curr[0] != words[i - 1][-1]:
+                return finish_game(i, n)
+
+    return [0, 0]
