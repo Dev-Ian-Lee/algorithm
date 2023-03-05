88eedffa212b056d553292bb2c9fe8b57e994b02
desfox <desfox99@naver.com>
2023-03-05 17:21:13 +0900

    Solve: Programmers 59412(lv2)

---
+++ programmers/level2/get_outtime.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59412
+
+SELECT HOUR(DATETIME) AS HOUR, COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR
+HAVING HOUR BETWEEN 9 AND 19
+ORDER BY HOUR
