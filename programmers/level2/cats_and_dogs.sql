68821a02a753dcc8b1059ed921c8f0daad16ec4d
desfox <desfox99@naver.com>
2023-03-07 16:52:42 +0900

    Solve: Programmers 59040(lv2)

---
+++ programmers/level2/cats_and_dogs.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59040
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) AS count
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+HAVING ANIMAL_TYPE = 'Dog'
+OR ANIMAL_TYPE = 'Cat'
+ORDER BY ANIMAL_TYPE
