94c026d7efaa4fd66a5e119f3d6fade58ba9f5a4
desfox <desfox99@naver.com>
2023-03-12 23:32:36 +0900

    Solve: Programmers 131113(lv3)

---
+++ programmers/level3/print_order_status.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131113
+
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, "%Y-%m-%d") AS OUT_DATE,
+(CASE WHEN DATEDIFF("2022-05-01", OUT_DATE) >= 0 THEN "출고완료"
+ WHEN DATEDIFF("2022-05-01", OUT_DATE) < 0 THEN "출고대기"
+ WHEN OUT_DATE IS NULL THEN "출고미정" END) AS "출고여부"
+FROM FOOD_ORDER
+ORDER BY ORDER_ID
