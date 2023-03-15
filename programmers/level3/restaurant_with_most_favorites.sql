c49d87323b87613aeacd1861125ad9f2ffe77913
desfox <desfox99@naver.com>
2023-03-15 23:18:40 +0900

    Solve: Programmers 131123(lv3)

---
+++ programmers/level3/restaurant_with_most_favorites.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131123
+
+SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+FROM REST_INFO A
+WHERE FAVORITES = (SELECT MAX(FAVORITES)
+                   FROM REST_INFO B
+                   WHERE A.FOOD_TYPE = B.FOOD_TYPE)
+GROUP BY FOOD_TYPE
+ORDER BY FOOD_TYPE DESC
