83043d0a7e016cb4a6a5b367a1cfa5819d7ea4b9
desfox <desfox99@naver.com>
2023-07-04 19:23:27 +0900

    MOD: BOJ 2512(S3)

--- boj/silver/2512.py
+++ boj/silver/2512.py
@@ -16,6 +16,8 @@ while True:
     mid = (start + end) // 2
 
     total = 0
+
+    # 요청된 예산이 mid보다 작을 경우에는 요청된 예산을, 더 클 경우 mid를 total에 누적
     for city in cities:
         if city < mid:
             total += city
@@ -23,10 +25,13 @@ while True:
         else:
             total += mid
 
+    # total이 예산총액을 넘지 않았을 경우 더 큰 범위를 탐색
+    # 최적값이 나올 때까지 수행
     if total <= m:
         highest = mid
         start = mid + 1
 
+    # 넘었을 경우 더 작은 범위를 탐색
     elif total > m:
         end = mid - 1
 
