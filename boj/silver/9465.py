9415d8d89dcd18b0ecefb4acb20b18e38caf4fdf
desfox <desfox99@naver.com>
2023-12-23 23:42:52 +0900

    MOD: BOJ 9465(S1)

--- boj/silver/9465.py
+++ boj/silver/9465.py
@@ -7,6 +7,8 @@
 # 행이 2로 고정이기 때문에 DP 테이블을 이중 리스트로 사용
 # DP의 경우 선택지를 반영할지 여부를 이지선다가 아닌 삼지선다로도 선택 가능
 
+# 각 열에서의 최대값을 계산하는 방식으로 접근
+
 import sys
 
 T = int(sys.stdin.readline())
@@ -19,18 +21,27 @@ for _ in range(T):
     for _ in range(2):
         stickers.append(list(map(int, sys.stdin.readline().rstrip().split())))
             
-    # dp는 각 열의 최대값을 의미
-    # stickers의 0번 인덱스까지만을 고려할 때, 각 열의 첫 번째 항목이 0열에서의 최대값
-    dp[0][0] = stickers[0][0]
-    dp[1][0] = stickers[1][0]
-    
-    # 1열에서의 최대값의 경우, 0열에서 위(아래) 칸을 뗐을 경우 그 대각선인 아래(위) 칸을 뗀 값 
-    dp[0][1] = dp[1][0] + stickers[0][1]
-    dp[1][1] = dp[0][0] + stickers[1][1]
-    
-    # 2열부터는 
-    for i in range(2, n):
-        dp[0][i] = stickers[0][i] + max(dp[1][i - 1], dp[0][i - 2], dp[1][i - 2])
-        dp[1][i] = stickers[1][i] + max(dp[0][i - 1], dp[0][i - 2], dp[1][i - 2])
+    if n == 1:
+        print(max(stickers[0][0], stickers[1][0]))
         
-    print(max(dp[0][n - 1], dp[1][n - 1]))
+    elif n == 2:
+        print(max(stickers[0][0] + stickers[1][1], stickers[0][1] + stickers[1][0]))
+        
+    else:
+            
+        # dp는 각 열의 최대값을 의미
+        # stickers의 0번 인덱스까지만을 고려할 때, 각 열의 첫 번째 항목이 0열에서의 최대값
+        dp[0][0] = stickers[0][0]
+        dp[1][0] = stickers[1][0]
+        
+        # 1열에서의 최대값의 경우, 0열에서 위(아래) 칸을 뗐을 경우 그 대각선인 아래(위) 칸을 더한 값
+        dp[0][1] = dp[1][0] + stickers[0][1]
+        dp[1][1] = dp[0][0] + stickers[1][1]
+        
+        # 2열부터의 최대값은 이전 열까지의 대각선값, 0행의 전전 열까지의 핪, 1행의 전전 열까지의 합 중 가장 큰 값에 현재 스티커 값을 더한 값
+        # 이전 열에서 아무것도 선택하지 않고 현재 스티커를 뗐을 때 최대값일 수도 있기 때문에, 전전 열까지 고려
+        for i in range(2, n):
+            dp[0][i] = stickers[0][i] + max(dp[1][i - 1], dp[0][i - 2], dp[1][i - 2])
+            dp[1][i] = stickers[1][i] + max(dp[0][i - 1], dp[0][i - 2], dp[1][i - 2])
+            
+        print(max(dp[0][n - 1], dp[1][n - 1]))
